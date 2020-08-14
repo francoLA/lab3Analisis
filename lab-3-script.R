632c0f32d6700ebb0087cf69fcd4a252ddb7b92b
franco <franco.labra@usach.cl>
2020-08-13 20:17:53 -0400

    lectura

---
+++ lab-3-script.R
@@ -0,0 +1,21 @@
+#
+library(arules)
+library(arulesViz)
+library(colorspace)
+# Guardamos la URL de la base de datos
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
+
+# Leemos directamente sobre los datos y se guarda en un data frame
+# El separador de columas es una coma ","
+cars <- read.table(url, sep = ",")
+
+# Colocamos los nombres a las columnas
+colnames(cars) <- c("buyingPrice", 
+                    "maintenanceCost", 
+                    "numberOfDoors", 
+                    "numberOfPersons",
+                    "sizeOfLuggageBoot",
+                    "safety",
+                    "decision")
+
+head(cars)
